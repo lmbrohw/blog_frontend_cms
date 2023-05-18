bed53d5bd6e00e13e1292ed4742ffe0423ed2edf
tang <asinsiner@foxmail.com>
2023-05-18 18:40:24 +0800

    修改请求路由为本机地址; 修改站点设置，去掉页脚徽标，去掉copyright等

--- src/views/page/SiteSetting.vue
+++ src/views/page/SiteSetting.vue
@@ -10,7 +10,8 @@
                         <span>基础设置</span>
                     </div>
                     <el-form label-position="right" label-width="100px">
-                        <el-form-item :label="item.nameZh" v-for="item in typeMap.type1" :key="item.id">
+                        <el-form-item :label="item.nameZh" v-for="item in typeMap.type1"
+                                      v-if="item.id !== 5 && item.id !== 6" :key="item.id">
                             <el-input v-model="item.value"></el-input>
                         </el-form-item>
                     </el-form>
@@ -39,35 +40,6 @@
                     </el-form>
                 </el-card>
             </el-col>
-        </el-row>
-
-        <el-row style="margin-top: 20px">
-            <el-card>
-                <div slot="header">
-                    <span>页脚徽标</span>
-                </div>
-                <el-form :inline="true" v-for="badge in typeMap.type2" :key="badge.id">
-                    <el-form-item label="title">
-                        <el-input v-model="badge.value.title"></el-input>
-                    </el-form-item>
-                    <el-form-item label="url">
-                        <el-input v-model="badge.value.url"></el-input>
-                    </el-form-item>
-                    <el-form-item label="subject">
-                        <el-input v-model="badge.value.subject"></el-input>
-                    </el-form-item>
-                    <el-form-item label="value">
-                        <el-input v-model="badge.value.value"></el-input>
-                    </el-form-item>
-                    <el-form-item label="color">
-                        <el-input v-model="badge.value.color"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="danger" icon="el-icon-delete" @click="deleteBadge(badge)">删除</el-button>
-                    </el-form-item>
-                </el-form>
-                <el-button type="primary" size="mini" icon="el-icon-plus" @click="addBadge">添加 badge</el-button>
-            </el-card>
         </el-row>
 
         <div style="text-align: right;margin-top: 30px">
